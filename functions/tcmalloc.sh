c57dfb04bde2d65e9122afbaac5c7ebf6fad3898
anthony <anthony@VM_88_203_centos.(none)>
2014-11-15 00:13:32 +0800

    add all

---
+++ functions/tcmalloc.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# Author:  yeho <lj2007331 AT gmail.com>
+# Blog:  http://blog.linuxeye.com
+
+Install_tcmalloc()
+{
+cd $lnmp_dir/src
+. ../functions/download.sh
+
+src_url=https://googledrive.com/host/0B6NtGsLhIcf7MWxMMF9JdTN3UVk/gperftools-2.2.90.tar.gz && Download_src
+
+tar xzf gperftools-2.2.90.tar.gz
+cd gperftools-2.2.90
+./configure --enable-frame-pointers
+make && make install
+cd ..
+/bin/rm -rf gperftools-2.2.90
+
+if [ -f "/usr/local/lib/libtcmalloc.so" ];then
+	echo '/usr/local/lib' > /etc/ld.so.conf.d/local.conf
+	ldconfig
+else
+	echo -e "\033[31mgperftools install failed, Please contact the author! \033[0m"
+	kill -9 $$
+fi
+cd ..
+}
